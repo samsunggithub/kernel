814973f1545c22034931031055690921fd7d2b1f
PhiHung113 <153968584+PhiHung113@users.noreply.github.com>
2024-01-20 11:45:39 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,7 +6,7 @@ export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
 
 make ARCH=arm64 exynos9820-d2s_defconfig
-make ARCH=arm64 -j64
+make ARCH=arm64 -j64 2>&1 | tee build.log
 
     rm -f ramdisk/split_img/boot.img-kernel
     cp arch/arm64/boot/Image ramdisk/split_img/boot.img-kernel
