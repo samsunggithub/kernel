e399e80fdda0bc5047acf512a4d014ccf6cf83ef
PhiHung113 <trantrongphihungdn1992@gmail.com>
2024-01-21 21:13:36 +0700

    new

--- lib/libdss.c
+++
@@ -1,6 +0,0 @@
-Traceback (most recent call last):
-  File "/home/phihung/a/lib/make_libdss.py", line 438, in <module>
-    astdump.prepare()
-  File "/home/phihung/a/lib/make_libdss.py", line 420, in prepare
-    self.dump = self.dump.strip().split("\n")
-TypeError: a bytes-like object is required, not 'str'
